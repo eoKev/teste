386c4d3fdb3c99f43f1ba806600fbf954bd60f55
eokev <cavalcante.kcg@gmail.com>
2024-11-19 21:49:33 -0300

    criação do appdatabase

---
+++ app/src/main/java/com/example/musicwhisky1/model/AppDB.kt
@@ -0,0 +1,37 @@
+package com.example.mvvm2.model.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.musicwhisky.model.AlbumDao
+import com.example.musicwhisky.model.ArtistaDao
+import com.example.musicwhisky.model.MusicaDao
+import com.example.musicwhisky.model.Musica
+import com.example.musicwhisky.model.Album
+import com.example.musicwhisky.model.Artista
+
+@Database(entities = [Artista::class, Musica:class, Album::class], version = 1)
+abstract class AppDB : RoomDatabase() {
+
+    abstract fun artistaDao(): ArtistaDao
+    abstract fun musicaDao(): MusicaDao
+    abstract fun albumDao(): AlbumDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDB? = null
+
+        fun getDatabase(context: Context): AppDB {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDB::class.java,
+                    "musicwhisky.db"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
