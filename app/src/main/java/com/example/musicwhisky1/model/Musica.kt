c4515a1e9e89ac9820793e9beb44391393812759
eokev <cavalcante.kcg@gmail.com>
2024-11-24 20:34:21 -0300

    correção na classe musica

--- app/src/main/java/com/example/musicwhisky1/model/Musica.kt
+++ app/src/main/java/com/example/musicwhisky1/model/Musica.kt
@@ -5,32 +5,31 @@ import androidx.room.ForeignKey
 import androidx.room.Index
 import androidx.room.PrimaryKey
 import com.example.musicwhisky.Album
-
-@Entity(tableName = "musicas",
+@Entity(
+    tableName = "musicas",
     foreignKeys = [
-        ForeignKey(
-            entity = Artista::class, // classe na qual esta sendo pega a chave estrangeira
-            parentColumns = ["id"], // coluna na tabela Artistas
-            childColumns = ["idArtista"], // coluna na tabela Musica
-            onDelete = ForeignKey.CASCADE // Define o comportamento em caso de delet de artista, o mesmo sera removido da tabela Musicas
-        ),
         ForeignKey(
             entity = Album::class,
             parentColumns = ["id"],
             childColumns = ["idAlbum"],
             onDelete = ForeignKey.CASCADE
+        ),
+        ForeignKey(
+            entity = Artista::class,
+            parentColumns = ["id"],
+            childColumns = ["idArtista"],
+            onDelete = ForeignKey.CASCADE
         )
     ],
-    indices = [Index(value = ["idArtista"]),Index(value = ["idAlbum"])]
-
+    indices = [Index(value = ["idAlbum"]), Index(value = ["idArtista"])]
 )
-data class Musica (
-    @PrimaryKey (autoGenerate = true )
+data class Musica(
+    @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
-    @ColumnInfo(name = "idArtista")
-    val idArtista: Int,
     @ColumnInfo(name = "idAlbum")
     val idAlbum: Int,
+    @ColumnInfo(name = "idArtista")
+    val idArtista: Int,
     val nome: String,
-    val duracao: String,
+    val duracao: Int
 )
