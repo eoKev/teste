df3b243676231539c21e3991ec6534af87450aa4
eokev <cavalcante.kcg@gmail.com>
2024-11-24 20:12:32 -0300

    ajuste na versao do database

--- app/src/main/java/com/example/musicwhisky1/database/AppDB.kt
+++ app/src/main/java/com/example/musicwhisky1/database/AppDB.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.example.musicwhisky.Album
 import com.example.musicwhisky1.dao.AlbumDao
 import com.example.musicwhisky1.dao.ArtistaDao
@@ -11,7 +13,7 @@ import com.example.musicwhisky1.dao.MusicaDao
 import com.example.musicwhisky1.model.Musica
 import com.example.musicwhisky1.model.Artista
 
-@Database(entities = [Artista::class, Musica::class, Album::class], version = 1)
+@Database(entities = [Artista::class, Musica::class, Album::class], version = 3)
 abstract class AppDB : RoomDatabase() {
 
     abstract fun artistaDao(): ArtistaDao
@@ -28,10 +30,14 @@ abstract class AppDB : RoomDatabase() {
                     context.applicationContext,
                     AppDB::class.java,
                     "musicwhisky.db"
-                ).build()
+                )
+                    // Usar fallbackToDestructiveMigration para recriar o banco de dados a cada mudança de versão
+                    .fallbackToDestructiveMigration() // Isso garante que o banco de dados será excluído e recriado
+                    .build()
                 INSTANCE = instance
                 instance
             }
         }
     }
 }
+
