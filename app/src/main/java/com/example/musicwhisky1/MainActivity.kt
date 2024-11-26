fad1d1d230dc083e4e64538cf2075b179db4f521
eokev <cavalcante.kcg@gmail.com>
2024-11-26 13:12:03 -0300

    atualização da main para instanciar o spotifyviewmodel

--- app/src/main/java/com/example/musicwhisky1/MainActivity.kt
+++ app/src/main/java/com/example/musicwhisky1/MainActivity.kt
@@ -8,29 +8,35 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.rememberNavController
 import com.example.musicwhisky1.ui.theme.Musicwhisky1Theme
 import com.example.musicwhisky1.view.AppNavigation
+import com.example.musicwhisky1.viewmodel.SpotifyVM
+import com.example.musicwhisky1.viewmodel.SpotifyVMFactory
 import com.example.mvvm2.model.database.AppDB
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         val database = AppDB.getDatabase(applicationContext)
         val artistaDao = database.artistaDao()
         val albumDao = database.albumDao()
         val musicaDao = database.musicaDao()
 
+
         setContent {
-            Musicwhisky1Theme (darkTheme = true){
+            Musicwhisky1Theme(darkTheme = true) {
+
                 val navController = rememberNavController()
+                val spotifyVM: SpotifyVM = viewModel(factory = SpotifyVMFactory())
 
                 AppNavigation(
                     navController = navController,
                     artistaDao = artistaDao,
                     albumDao = albumDao,
-                    musicaDao = musicaDao
+                    musicaDao = musicaDao,
+                    spotifyVM = spotifyVM
                 )
             }
         }
