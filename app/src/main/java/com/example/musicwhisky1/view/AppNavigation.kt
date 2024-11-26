76d298c71bd975225ed034f357487570fd195ba9
eokev <cavalcante.kcg@gmail.com>
2024-11-26 13:03:52 -0300

    alteração no appnavigation para implementar a tela de exibição de consulta de playlists da api do spotify

--- app/src/main/java/com/example/musicwhisky1/view/AppNavigation.kt
+++ app/src/main/java/com/example/musicwhisky1/view/AppNavigation.kt
@@ -5,74 +5,69 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import androidx.room.Room
+import com.example.musicwhisky1.api.PlaylistDetailsScreen
 import com.example.musicwhisky1.dao.AlbumDao
 import com.example.musicwhisky1.dao.ArtistaDao
 import com.example.musicwhisky1.dao.MusicaDao
-import com.example.musicwhisky1.view.TelaInicial
 import com.example.musicwhisky1.view.TelaGerenciamento
 import com.example.musicwhisky1.viewmodel.AlbumVMFactory
 import com.example.musicwhisky1.viewmodel.ArtistaVMFactory
 import com.example.musicwhisky1.viewmodel.MusicaVMFactory
+import com.example.musicwhisky1.viewmodel.SpotifyVM
 import com.example.mvvm2.model.database.AppDB
 import com.example.mvvm2.viewmodel.AlbumVM
 import com.example.mvvm2.viewmodel.ArtistaVM
 import com.example.mvvm2.viewmodel.MusicaVM
-
 
 @Composable
 fun AppNavigation(
     navController: NavHostController,
     artistaDao: ArtistaDao,
     albumDao: AlbumDao,
-    musicaDao: MusicaDao
+    musicaDao: MusicaDao,
+    spotifyVM: SpotifyVM
 ) {
     NavHost(navController = navController, startDestination = "home") {
 
-        // Tela inicial
         composable("home") {
             TelaInicial(navController)
         }
 
-        // Tela de cadastro de artista
-        composable("gerenciamentoArtista") {
-            val artistaFactory = ArtistaVMFactory(artistaDao)
-            val artistaVM: ArtistaVM = viewModel(factory = artistaFactory)
-            TelaGerenciamentoArtista(navController, artistaVM)
-        }
-
-        // Tela de cadastro de álbum
-        composable("gerenciamentoAlbum") {
-            val albumFactory = AlbumVMFactory(albumDao, artistaDao)
-            val albumVM: AlbumVM = viewModel(factory = albumFactory)
-
-            // Instanciando o ArtistaVM diretamente
-            val artistaFactory = ArtistaVMFactory(artistaDao)
-            val artistaVM: ArtistaVM = viewModel(factory = artistaFactory)
-
-            TelaGerenciamentoAlbum(navController, albumVM, artistaVM)
-        }
-
-        // Tela de gerenciamento geral
         composable("gerenciamento") {
             TelaGerenciamento(navController)
         }
 
-        // Tela de cadastro de música
+        composable("gerenciamentoArtista") {
+            val artistaVM: ArtistaVM = viewModel(factory = ArtistaVMFactory(artistaDao))
+            TelaGerenciamentoArtista(navController, artistaVM)
+        }
+
+        composable("gerenciamentoAlbum") {
+            val artistaVM: ArtistaVM = viewModel(factory = ArtistaVMFactory(artistaDao))
+            val albumVM: AlbumVM = viewModel(factory = AlbumVMFactory(albumDao, artistaDao))
+            TelaGerenciamentoAlbum(navController, albumVM, artistaVM)
+        }
+
         composable("gerenciamentoMusica") {
-            val albumFactory = AlbumVMFactory(albumDao, artistaDao)
-            val albumVM: AlbumVM = viewModel(factory = albumFactory)
 
-            val artistaFactory = ArtistaVMFactory(artistaDao)
-            val artistaVM: ArtistaVM = viewModel(factory = artistaFactory)
-
-            val musicaFactory = MusicaVMFactory(musicaDao, albumDao, artistaDao)
-            val musicaVM: MusicaVM = viewModel(factory = musicaFactory)
-
+            val artistaVM: ArtistaVM = viewModel(factory = ArtistaVMFactory(artistaDao))
+            val albumVM: AlbumVM = viewModel(factory = AlbumVMFactory(albumDao, artistaDao))
+            val musicaVM: MusicaVM = viewModel(factory = MusicaVMFactory(musicaDao, albumDao, artistaDao))
             TelaGerenciamentoMusica(navController, musicaVM, artistaVM, albumVM)
+        }
+
+        composable(
+            route = "playlistTracks/{playlistId}",
+            arguments = listOf(navArgument("playlistId") { type = NavType.StringType })
+        ) { backStackEntry ->
+            val playlistId = backStackEntry.arguments?.getString("playlistId").orEmpty()
+            PlaylistDetailsScreen(navController, playlistId)
         }
     }
 }
