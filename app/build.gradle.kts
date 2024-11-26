788a63ea2ff4da7b0993d576536d6fcb6aef6ae4
eokev <cavalcante.kcg@gmail.com>
2024-11-26 15:22:59 -0300

    Teste

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,10 +69,12 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
 
-    // Navigation do JetpackCompose
+    // Jetpack Compose
     implementation ("androidx.navigation:navigation-compose:2.8.0")
 
+
     // Import do coil-compose para exibir a imagem das playlists
+
     implementation("io.coil-kt:coil-compose:2.2.2")
     implementation("androidx.compose.material3:material3:1.0.0")
     implementation("androidx.activity:activity-compose:1.7.0")
