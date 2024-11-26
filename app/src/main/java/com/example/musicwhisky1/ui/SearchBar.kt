807be1d6cde2fcf9daac364a98f47063a0c754f6
eokev <cavalcante.kcg@gmail.com>
2024-11-26 13:06:48 -0300

    atualização na classe searchbar e criação da artistadialog para retornar busca de nomes de artistas feitas pela api

--- app/src/main/java/com/example/musicwhisky1/ui/SearchBar.kt
+++ app/src/main/java/com/example/musicwhisky1/ui/SearchBar.kt
@@ -1,32 +1,26 @@
 package com.example.musicwhisky1.ui
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.text.TextStyle
 
 @Composable
 fun SearchBar(
     query: String,
     onQueryChange: (String) -> Unit,
+    onSearch: (String) -> Unit,
     modifier: Modifier = Modifier
 ) {
     Column(
@@ -35,46 +29,36 @@ fun SearchBar(
             .padding(top = 18.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Box(
-            contentAlignment = Alignment.Center
-        ) {
-            TextField(
-                value = query,
-                onValueChange = onQueryChange,
-                placeholder = {
-                    Text(
-                        text = "Pesquisar...",
-                        fontSize = 12.sp,
-                    )
-                },
-                modifier = Modifier
-                    .fillMaxWidth(0.6f),
-                singleLine = true,
-                leadingIcon = {
-                    Icon(
-                        imageVector = Icons.Default.Search,
-                        contentDescription = "Pesquisar",
-                        modifier = Modifier.size(14.dp)
-                    )
-                },
-                shape = RoundedCornerShape(15.dp),
-                textStyle = TextStyle(
-                    color = Color.White,
-                    fontSize = 14.sp
-                ),
-            )
-        }
+        TextField(
+            value = query,
+            onValueChange = onQueryChange,
+            placeholder = {
+                Text(
+                    text = "Pesquisar...",
+                    fontSize = 12.sp,
+                )
+            },
+            modifier = Modifier
+                .fillMaxWidth(0.6f),
+            singleLine = true,
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = "Pesquisar",
+                    modifier = Modifier.size(14.dp)
+                )
+            },
+            shape = RoundedCornerShape(15.dp),
+            textStyle = TextStyle(
+                color = Color.White,
+                fontSize = 14.sp
+            ),
+            keyboardOptions = KeyboardOptions.Default.copy(
+                imeAction = ImeAction.Search
+            ),
+            keyboardActions = KeyboardActions(
+                onSearch = { onSearch(query) }
+            ),
+        )
     }
-}
-
-
-
-@Preview(showBackground = true)
-@Composable
-fun SearchBarPreview() {
-    var query by remember { mutableStateOf("") }
-    SearchBar(
-        query = query,
-        onQueryChange = { newQuery -> query = newQuery }
-    )
 }
